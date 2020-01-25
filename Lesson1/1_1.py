d88c70e370ed26c849cc6c39c4f449ae76400539
Pavel Vyukov_W <vukov87@mail.ru>
2020-01-25 14:26:48 +0300

    First task is done

--- Lesson1/1_1.py
+++ Lesson1/1_1.py
@@ -1,1 +1,9 @@
-print ('Hello!!')
+a = 3
+word = 'lol'
+
+print(f'number is {a} and word is {word}')
+
+a = int(input('Enter number: '))
+word = input('Enter 1 word: ')
+
+print(f'number is {a} and word is {word}')
