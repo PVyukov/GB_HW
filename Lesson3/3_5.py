da33bf8a67af750b96c4987d6a11c5ba5eb979db
Pavel Vyukov_W <vukov87@mail.ru>
2020-01-31 00:29:30 +0300

    3_5 task done_improved

--- Lesson3/3_5.py
+++ Lesson3/3_5.py
@@ -3,7 +3,7 @@ flag = True
 while flag:
     a = input('Enter numbers(Pres "F" to finish): ')
     my_list = a.split()
-    if my_list[len(my_list)-1] == 'F':
+    if 'F' in my_list:
         flag = False
         my_list.remove('F')
     my_sum += sum(map(int, my_list))
