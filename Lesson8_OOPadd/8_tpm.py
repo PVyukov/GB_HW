1884ae665adc2a3a745192b4a076b9048c0e3725
Pavel Vyukov_W <vukov87@mail.ru>
2020-04-05 18:52:47 +0300

    HW 8. tmp file was added

---
+++ Lesson8_OOPadd/8_tpm.py
@@ -0,0 +1,34 @@
+# class Fruit:
+#     name = 'Fruitas'
+#
+#     @classmethod
+#     def printName(cls):
+#         print('The name is:', cls.name)
+# # Fruit.printName()
+# apple = Fruit()
+# berry = Fruit()
+# #
+# # Fruit.printName()
+# # apple.printName()
+# # berry.printName()
+#
+#
+# apple.name="Apple"
+# Fruit.printName()
+# apple.printName()
+# berry.printName()
+#
+# Fruit.name="Apple"
+# Fruit.printName()
+# apple.printName()
+# berry.printName()
+
+
+# a = [i for i in range(100)]
+# while True:
+#     e = 0
+
+
+import  subprocess
+
+[].ite
